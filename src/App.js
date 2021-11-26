f9a5116c53da3d978527842f163517df16dd10ce
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-11-26 21:54:00 +0000

    Organize files into UI and Expenses folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
 	const expenses = [
 		{
 			title: "Toilet Paper",
@@ -24,6 +24,6 @@ function App() {
 			<Expenses expenses={[...expenses]} />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 import "./ExpenseDate.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
 	const month = props.date.toLocaleString("en-US", { month: "long" });
 	const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 	const year = props.date.getFullYear();
@@ -12,6 +12,6 @@ function ExpenseDate(props) {
 			<div className="expense-item__date-day">{day}</div>
 		</Card>
 	);
-}
+};
 
 export default ExpenseDate;
