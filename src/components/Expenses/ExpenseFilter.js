8c110a52743fe49f57fa7743cfddcc0f7796806e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-11-29 21:28:22 +0000

    Add expnse fiter

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import "./ExpenseFilter.css"
+
+const ExpenseFilter = () => {
+	return (
+		<div className="expense-filter">
+			<label>Filter by year</label>
+			<option></option>
+		</div>
+	)
+}
