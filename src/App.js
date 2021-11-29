b5d532589af243afa48fc1f91190d3264ff10c49
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-11-29 15:36:28 +0000

    Add NewExpense form UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpenses/NewExpense";
 const App = () => {
 	const expenses = [
 		{
@@ -20,7 +20,7 @@ const App = () => {
 	];
 	return (
 		<div className="App">
-			<h2>Let's get started</h2>
+			<NewExpense />
 			<Expenses expenses={[...expenses]} />
 		</div>
 	);
