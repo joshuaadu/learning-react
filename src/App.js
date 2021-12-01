859e403e684b747560c4c317e64aa03650e2ed84
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-12-01 16:55:30 +0000

    Add id keys to dummy expenses

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import { useState } from "react"
-import Expenses from "./components/Expenses/Expenses"
-import NewExpense from "./components/NewExpenses/NewExpense"
+import { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpenses/NewExpense";
 
 const DUMMY_EXPENSES = [
 	{
 		title: "Toilet Paper",
 		amount: 98.4,
 		date: new Date(2020, 7, 14),
+		id: "e1",
 	},
 	{
 		title: "Car Insurance",
 		amount: 198.4,
 		date: new Date(2020, 7, 14),
+		id: "e1",
 	},
 	{
 		title: "Car Maintenance",
 		amount: 298.4,
 		date: new Date(2020, 7, 14),
+		id: "e1",
 	},
-]
+];
 
 const App = () => {
-	const [expenses, setAddExpenses] = useState(DUMMY_EXPENSES)
+	const [expenses, setAddExpenses] = useState(DUMMY_EXPENSES);
 
 	const addExpenseHandler = (expense) => {
-		setAddExpenses((prevExpenses) => [expense, ...prevExpenses])
-	}
-	console.log(expenses)
+		setAddExpenses((prevExpenses) => [expense, ...prevExpenses]);
+	};
+	console.log(expenses);
 
 	return (
 		<div className="App">
 			<NewExpense onAddExpense={addExpenseHandler} />
 			<Expenses expenses={expenses} />
 		</div>
-	)
-}
+	);
+};
 
-export default App
+export default App;
